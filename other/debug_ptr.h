edc620f83f525f3cd7b5d0892bb05574110cebcd
Stefano Brilli <stefano@tails.homenet.telecomitalia.it>
2012-08-19 11:23:01 +0200

    Uses templates to reduce the amount of code and classes. Adds some comments to the code.

--- other/debug_ptr.h
+++ other/debug_ptr.h
@@ -1,35 +1,103 @@
+// Author: Stefano Brilli - stefanobrilli@gmail.com
+// Licensing: see README file in the root dir of this project.
+//
+// debug_ptr is a class designed to be used in place of standard pointers
+// for storing the address of dinamically allocated Objects and Object arrays.
+// It helps to spot memory leaks in your software by
+// keeping track of the references to a dynamically allocated object (or array)
+// and, when all references are lost, checks if the object was deleted. If not,
+// an event is raised (depending on the choosen policy). 
+
 #ifndef DEBUG_PTR_H
 #define DEBUG_PTR_H
 #include <iostream>
+#include <stdexcept>
 
-namespace debug_types {
-template <typename T> class debug_ptr;
-template <typename T> class debug_array;
-}
+#ifndef ENABLE_DEBUG_TYPES
+#define typedef_pointer(Type, Alias, ...) typedef Type Alias
+#define typedef_array(Type, Alias, ...) typedef Type Alias
 
-#ifdef ENABLE_DEBUG_TYPES
-#define typedef_pointer(Type, Alias) typedef debug_types::debug_ptr< Type > Alias
-#define typedef_array(Type, Alias) typedef debug_types::debug_array< Type > Alias
-#else
-#define typedef_pointer(Type, Alias) typedef Type* Alias
-#define typedef_array(Type, Alias) typedef Type* Alias
+#else // ENABLE_DEBUG_TYPES
+
+// Defines the Policy class to call in case of Leak detection
+#ifndef DEBUG_TYPES_POLICY
+#define DEBUG_TYPES_POLICY PrintPolicy
 #endif
 
+#define DEBUG_TYPES_JOIN_ARGS(...) __VA_ARGS__
+
+#define typedef_pointer(Type, Alias, ...) \
+typedef debug_types::debug_ptr<false, DEBUG_TYPES_JOIN_ARGS(Type, ##__VA_ARGS__) > Alias
+
+#define typedef_array(Type, Alias, ...) \
+typedef debug_types::debug_ptr<true, DEBUG_TYPES_JOIN_ARGS(Type, ##__VA_ARGS__) > Alias
 
 namespace debug_types {
-namespace internal {
-template <typename T> class shared_ptr_data {
-public:
-	static shared_ptr_data<T>* new_shared_ptr_data(T* ptr) {
-		return new shared_ptr_data<T>(ptr, 0);
+
+// This policy will print a warning message in case of memory leak
+struct PrintPolicy;
+
+// This policy will throw a std::runtime_error in case of memory leak
+struct ThrowPolicy;
+
+// Declaration of the debug_ptr class. Note that this class is not actually defined.
+// This is to avoid instantiation of debug_ptr for non pointer types.
+template <bool Array, typename T, typename Policy=DEBUG_TYPES_POLICY> class debug_ptr;
+// This forward declares the debug_ptr class for Pointer types.
+template <bool Array, typename T, typename Policy> class debug_ptr<Array, T*, Policy>;
+
+// This is the class that will delete the object and "mark" the object as deleted
+// (by setting to 0 its )
+template <bool Array, typename T, typename Policy> class deleter;
+}
+
+namespace debug_types {
+
+struct ThrowPolicy {
+	ThrowPolicy() {
+		throw std::runtime_error("lost reference to undeleted object");
 	}
+};
+
+struct PrintPolicy {
+	PrintPolicy() {
+		std::cerr << "WARNING: lost reference "
+					 "to undeleted object" << std::endl;
+	}
+};
+
+namespace internal {
+
+// An instance of this class is referenced by one or more debug_ptr(s).
+// It contains the dynamically allocated object addess and a counter of
+// debug_ptr(s) that are referencing the class instance.
+// If the counter goes to 0 means that no debug_ptr has any reference
+// to this instance. Hence the object address is checked to see if 
+// it was set to NULL (and deleted) by the deleter class. If the object
+// was not deleted, the the Policy is called. Finally the instance delete
+// itself.
+
+template <typename T, typename Policy> class shared_ptr_data {
+	// Only the deleter class can set to 0 the inner pointer
+	friend class deleter<true, T, Policy>;
+	friend class deleter<false, T, Policy>;
+public:
+	static shared_ptr_data<T, Policy>* new_shared_ptr_data(T* ptr) {
+		return new shared_ptr_data<T, Policy>(ptr, 0);
+	}
+
+	// Atomic increment
+	// FIXME: if no threads are used, this could be just ++value_
 	void IncrementRefCounter() {
 		__sync_add_and_fetch(&value_, 1);
 	}
+
+	// Atomic decrement
+	// FIXME: if no threads are used, this could be just --value_
 	void DecrementRefCounter() {
 		if (__sync_add_and_fetch(&value_, -1) == 0) {
 			if (pointer_ != NULL)
-				throw;
+				Policy();
 			delete this;
 		}
 			
@@ -39,54 +107,73 @@ public:
 
 private:
 	shared_ptr_data(T* ptr, int c) : pointer_(ptr), value_(c) {}
-	shared_ptr_data(const shared_ptr_data<T>&);
+	shared_ptr_data(const shared_ptr_data<T, Policy>&);
 
-	shared_ptr_data& operator=(const shared_ptr_data<T>&);
+	shared_ptr_data& operator=(const shared_ptr_data<T, Policy>&);
 
 	T* pointer_;
 	int value_;
 };
-} // End of namespace private
 
-template <typename T> class ptr_deleter {
+} // namespace internal
+
+// When deleter class is destroied it deletes the object referenced
+// in its shared_ptr_data. then Set to zero the address of that object
+template <bool Array, typename T, typename Policy> class deleter {
 public:
-	static ptr_deleter<T>* new_ptr_deleter(internal::shared_ptr_data<T>* spd) {
-		return new ptr_deleter<T>(spd);
+	static deleter<Array, T, Policy>* new_deleter(
+			internal::shared_ptr_data<T, Policy>* spd) {
+		if (Array) {
+			deleter<Array, T, Policy>* tmp = new deleter<Array, T, Policy>[1];
+			tmp[0].spd_ = spd;
+			return tmp;
+		} else {
+			return new deleter<Array, T, Policy>(spd);
+		}
 	}
 
-	~ptr_deleter() {
-		delete spd_->pointer();
-		spd_->set_pointer(NULL);
+	~deleter() {
+		if (Array) {
+			delete [] spd_->pointer();
+		} else {
+			delete spd_->pointer();
+		}
+		spd_->pointer_ = NULL;
 	}
 
 private:
-	ptr_deleter() : spd_(NULL) {};
-	ptr_deleter(internal::shared_ptr_data<T>* s) : spd_(s) {};
-	ptr_deleter(const ptr_deleter<T>&);
-	ptr_deleter<T>& operator=(const ptr_deleter&);
+	deleter() : spd_(NULL) {};
+	deleter(internal::shared_ptr_data<T, Policy>* s) : spd_(s) {};
+	deleter(const deleter<Array, T, Policy>&);
+	deleter<Array, T, Policy>& operator=(const deleter&);
 
-	internal::shared_ptr_data<T>* spd_;
+	internal::shared_ptr_data<T, Policy>* spd_;
 };
 
-template <typename T> class debug_ptr {
+// This class is the one that should replace the real pointer type.
+// Everytime that this class is copied or deleted, icreases or decreases 
+// the reference counter inside its shared_ptr_data.
+// when the delete or delete [] operator is called on this class, the cast
+// to delete* operator is automatically called.
+template <bool Array, typename T, typename Policy> class debug_ptr<Array, T*, Policy> {
 public:
 
 	debug_ptr() {
-		pd_ = internal::shared_ptr_data<T>::new_shared_ptr_data(NULL);
+		pd_ = internal::shared_ptr_data<T, Policy>::new_shared_ptr_data(NULL);
 		pd_->IncrementRefCounter();
 	}
 
 	debug_ptr(T* ptr) {
-		pd_ = internal::shared_ptr_data<T>::new_shared_ptr_data(ptr);
+		pd_ = internal::shared_ptr_data<T, Policy>::new_shared_ptr_data(ptr);
 		pd_->IncrementRefCounter();
 	}
 
-	debug_ptr(const debug_ptr<T>& o) {
+	debug_ptr(const debug_ptr<Array, T*, Policy>& o) {
 		o.pd_->IncrementRefCounter();
 		pd_ = o.pd_;
 	}
 
-	debug_ptr& operator=(const debug_ptr<T>& o) {
+	debug_ptr& operator=(const debug_ptr<Array, T*, Policy>& o) {
 		o.pd_->IncrementRefCounter();
 		pd_->DecrementRefCounter();
 		pd_ = o.pd_;
@@ -94,7 +181,7 @@ public:
 	}
 
 	debug_ptr& operator=(T* p) {
-		debug_ptr<T>(p).swap(this);
+		debug_ptr<Array, T*, Policy>(p).swap(this);
 		return *this;
 	}
 
@@ -106,102 +193,26 @@ public:
 	const T& operator*() const { return *pd_->pointer(); }
 	T* operator->() { return pd_->pointer(); }
 	const T* operator->() const { return pd_->pointer(); }
-
-	operator ptr_deleter<T>*() {
-		return ptr_deleter<T>::new_ptr_deleter(pd_);
-	}
-
-private:
-
-	void swap(debug_ptr<T>* o) {
-		internal::shared_ptr_data<T>* tmp = pd_;
-		pd_ = o->pd_;
-		o->pd_ = tmp;
-	}
-
-	internal::shared_ptr_data<T>* pd_;
-
-};
-
-
-template <typename T> class array_deleter {
-public:
-	static array_deleter<T>* new_array_deleter(internal::shared_ptr_data<T>* spd) {
-		array_deleter<T>* tmp = new array_deleter<T>[1];
-		tmp[0].spd_ = spd;
-		return tmp;
-	}
-
-	~array_deleter() {
-		delete [] spd_->pointer();
-		spd_->set_pointer(NULL);
-	}
-
-private:
-	array_deleter() : spd_(NULL) {};
-	array_deleter(internal::shared_ptr_data<T>* s) : spd_(s) {};
-	array_deleter(const array_deleter<T>&);
-	array_deleter<T>& operator=(const array_deleter&);
-
-	internal::shared_ptr_data<T>* spd_;
-};
-
-template <typename T> class debug_array {
-public:
-
-	debug_array() {
-		pd_ = internal::shared_ptr_data<T>::new_shared_ptr_data(NULL);
-		pd_->IncrementRefCounter();
-	}
-
-	debug_array(T* ptr) {
-		pd_ = internal::shared_ptr_data<T>::new_shared_ptr_data(ptr);
-		pd_->IncrementRefCounter();
-	}
-
-	debug_array(const debug_array<T>& o) {
-		o.pd_->IncrementRefCounter();
-		pd_ = o.pd_;
-	}
-
-	debug_array& operator=(const debug_array<T>& o) {
-		o.pd_->IncrementRefCounter();
-		pd_->DecrementRefCounter();
-		pd_ = o.pd_;
-		return *this;
-	}
-
-	debug_array& operator=(T* p) {
-		debug_array<T>(p).swap(this);
-		return *this;
-	}
-
-	~debug_array() {
-		pd_->DecrementRefCounter();
-	}
-
-	T& operator*() { return *pd_->pointer(); }
-	const T& operator*() const { return *pd_->pointer(); }
-	T* operator->() { return pd_->pointer(); }
-	const T* operator->() const { return pd_->pointer(); }
 	T& operator[](int offset) { return pd_->pointer()[offset]; }
 	const T& operator[](int offset) const { return pd_->pointer()[offset]; }
 
-	operator array_deleter<T>*() {
-		return array_deleter<T>::new_array_deleter(pd_);
+	operator deleter<Array, T, Policy>*() {
+		return deleter<Array, T, Policy>::new_deleter(pd_);
 	}
 
 private:
 
-	void swap(debug_array<T>* o) {
-		internal::shared_ptr_data<T>* tmp = pd_;
+	void swap(debug_ptr<Array, T*, Policy>* o) {
+		internal::shared_ptr_data<T, Policy>* tmp = pd_;
 		pd_ = o->pd_;
 		o->pd_ = tmp;
 	}
 
-	internal::shared_ptr_data<T>* pd_;
+	internal::shared_ptr_data<T, Policy>* pd_;
 
 };
 
 }
+#endif // ENABLE_DEBUG_TYPES
+
 #endif
