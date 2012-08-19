6736dbd6eedf0ec0666db4f6c623eace3d4f4a34
Stefano Brilli <stefano@tails.homenet.telecomitalia.it>
2012-08-19 11:34:40 +0200

    Update the test for debug_ptr

--- other/debug_ptr_test.cc
+++ other/debug_ptr_test.cc
@@ -1,25 +1,33 @@
-#define ENABLE_DEBUG_TYPES
 #include "debug_ptr.h"
 #include <cstdlib>
 #include <iostream>
 #include <vector>
 
-typedef_array(int, int_pa);
+typedef_pointer(int*, int_p);
+typedef_array(int_p*, int_pa);
+typedef_array(int_pa*, int_paa, debug_types::PrintPolicy);
 
 int main(int argc, char** argv) {
+
 	const int N = 10;
 	const int M = 10;
-
-	std::vector< int_pa > matrix;
+	int_paa other;
+	int_paa matrix = new int_pa[M];
 	for (int i = 0; i < M; ++i) {
-		matrix.push_back(new int[N]);
+		matrix[i] = new int_p[N];
 		for (int j=0; j < N; ++j) {
-			int_pa tmp = matrix.back();
-			tmp[j]=i*M+j;
+			int_pa tmp = matrix[i];
+			tmp[j] = new int(i*M+j);
 		}
 	}
 
-
-	for (int i = 0; i < M; ++i)
+	for (int i = 0; i < M; ++i) {
+		for (int j=0; j < N; ++j) {
+			delete matrix[i][j];
+		}
 		delete [] matrix[i];
+	}
+	// Try to comment the following line to see how this memory leak is handled
+	other = matrix;
+	delete [] other;
 }
